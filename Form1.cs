faf5cee6d05aefb5f516e0521b02ece59592e862
Brian Kozub <ticet11@gmail.com>
2021-09-29 21:48:38 -0600

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyBuddy
+{
+    public partial class FormDashboard : Form
+    {
+        public FormDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdShow_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
