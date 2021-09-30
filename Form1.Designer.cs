a5cfb480d40a70d40d94a61da93079a3a83af62c
Brian Kozub <ticet11@gmail.com>
2021-09-29 22:56:22 -0600

    MyBuddy can show, hide, speak, and move.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,11 +31,15 @@ namespace MyBuddy
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDashboard));
             this.CmdShow = new System.Windows.Forms.Button();
+            this.CmdHide = new System.Windows.Forms.Button();
+            this.CmdSpeak = new System.Windows.Forms.Button();
+            this.txtSpeak = new System.Windows.Forms.TextBox();
+            this.CmdMove = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CmdShow
             // 
-            this.CmdShow.Location = new System.Drawing.Point(100, 61);
+            this.CmdShow.Location = new System.Drawing.Point(153, 12);
             this.CmdShow.Name = "CmdShow";
             this.CmdShow.Size = new System.Drawing.Size(100, 40);
             this.CmdShow.TabIndex = 0;
@@ -43,22 +47,70 @@ namespace MyBuddy
             this.CmdShow.UseVisualStyleBackColor = true;
             this.CmdShow.Click += new System.EventHandler(this.CmdShow_Click);
             // 
+            // CmdHide
+            // 
+            this.CmdHide.Location = new System.Drawing.Point(47, 12);
+            this.CmdHide.Name = "CmdHide";
+            this.CmdHide.Size = new System.Drawing.Size(100, 40);
+            this.CmdHide.TabIndex = 1;
+            this.CmdHide.Text = "Hide";
+            this.CmdHide.UseVisualStyleBackColor = true;
+            this.CmdHide.Click += new System.EventHandler(this.CmdHide_Click);
+            // 
+            // CmdSpeak
+            // 
+            this.CmdSpeak.Location = new System.Drawing.Point(47, 107);
+            this.CmdSpeak.Name = "CmdSpeak";
+            this.CmdSpeak.Size = new System.Drawing.Size(100, 40);
+            this.CmdSpeak.TabIndex = 2;
+            this.CmdSpeak.Text = "Speak";
+            this.CmdSpeak.UseVisualStyleBackColor = true;
+            this.CmdSpeak.Click += new System.EventHandler(this.CmdSpeak_Click);
+            // 
+            // txtSpeak
+            // 
+            this.txtSpeak.Location = new System.Drawing.Point(47, 68);
+            this.txtSpeak.Name = "txtSpeak";
+            this.txtSpeak.Size = new System.Drawing.Size(206, 20);
+            this.txtSpeak.TabIndex = 3;
+            this.txtSpeak.Text = "Hi! Nice to see you!";
+            // 
+            // CmdMove
+            // 
+            this.CmdMove.Location = new System.Drawing.Point(153, 107);
+            this.CmdMove.Name = "CmdMove";
+            this.CmdMove.Size = new System.Drawing.Size(100, 40);
+            this.CmdMove.TabIndex = 4;
+            this.CmdMove.Text = "Move";
+            this.CmdMove.UseVisualStyleBackColor = true;
+            this.CmdMove.Click += new System.EventHandler(this.CmdMove_Click);
+            // 
             // FormDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(301, 163);
+            this.Controls.Add(this.CmdMove);
+            this.Controls.Add(this.txtSpeak);
+            this.Controls.Add(this.CmdSpeak);
+            this.Controls.Add(this.CmdHide);
             this.Controls.Add(this.CmdShow);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormDashboard";
             this.Text = "MyBuddy Control Board";
+            this.Load += new System.EventHandler(this.FormDashboard_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button CmdShow;
+        private System.Windows.Forms.Button CmdHide;
+        private System.Windows.Forms.Button CmdSpeak;
+        private System.Windows.Forms.TextBox txtSpeak;
+        private System.Windows.Forms.Button CmdMove;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using DoubleAgent.AxControl;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace MyBuddy
 {
     public partial class FormDashboard : Form
     {
+        AxControl newAgent;
         public FormDashboard()
         {
             InitializeComponent();
@@ -19,6 +21,53 @@ namespace MyBuddy
 
         private void CmdShow_Click(object sender, EventArgs e)
         {
+            newAgent.Characters["MyBuddy"].Show();
+        }
+
+        private void FormDashboard_Load(object sender, EventArgs e)
+        {
+            newAgent = new AxControl();
+            newAgent.CreateControl();
+            newAgent.Characters.Load("MyBuddy", "E:\\Coding Projects\\MyBuddy\\pikachu.acs");
+        }
+
+        private void CmdHide_Click(object sender, EventArgs e)
+        {
+            newAgent.Characters["MyBuddy"].Hide();
+        }
+
+        private void CmdSpeak_Click(object sender, EventArgs e)
+        {
+            newAgent.Characters["MyBuddy"].Speak(txtSpeak.Text);
+        }
+
+        private void CmdMove_Click(object sender, EventArgs e)
+        {
+            short screenWidth = (short)(Screen.PrimaryScreen.Bounds.Width - 100);
+            short screenHeight = (short)(Screen.PrimaryScreen.Bounds.Height - 100);
+
+            Random rnd = new Random();
+            int direction = rnd.Next(1, 5);
+
+            switch (direction)
+            {
+                // Top Left
+                case 1:
+                    newAgent.Characters["MyBuddy"].MoveTo(0, 0);
+                    break;
+                // Top Right
+                case 2:
+                    newAgent.Characters["MyBuddy"].MoveTo(screenWidth, 0);
+                    break;
+                // Bottom Left
+                case 3:
+                    newAgent.Characters["MyBuddy"].MoveTo(0, screenHeight);
+                    break;
+                // Bottom Right
+                case 4:
+                    newAgent.Characters["MyBuddy"].MoveTo(screenWidth, screenHeight);
+                    break;
+            }
 
         }
     }
