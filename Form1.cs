b93071c7ae8e87155bbb98abcc7e37d639e2c525
Brian Kozub <ticet11@gmail.com>
2021-10-05 13:40:15 -0600

    Removed all unneccessary packages and connections, including gmail specific integration.

--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using DoubleAgent.AxControl;
-using Google.Apis.Auth.OAuth2;
-using Google.Apis.Gmail.v1;
-using Google.Apis.Services;
-using Google.Apis.Util.Store;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using System.Windows.Forms;
 
 namespace MyBuddy
@@ -20,8 +7,6 @@ namespace MyBuddy
     public partial class FormDashboard : Form
     {
         AxControl newAgent;
-        string[] Scopes = { GmailService.Scope.GmailSend };
-        string ApplicationName = "MyBuddy";
 
         public FormDashboard()
         {
@@ -81,30 +66,9 @@ namespace MyBuddy
 
         }
 
-        string Base64UrlEncode(string input)
-        {
-            var data = Encoding.UTF8.GetBytes(input);
-            return Convert.ToBase64String(data).Replace("+", "-").Replace("/", "_").Replace("=", "");
-        }
-
         private void CmdSend_Click(object sender, EventArgs e)
         {
-            UserCredential userCred;
-            // read credentials file
-            using (FileStream stream = new FileStream(Application.StartupPath + @"/credentials.json", FileMode.Open, FileAccess.Read))
-            {
-                string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-                path = Path.Combine(path, ".credentials/gmail-dotnet-quickstart.json");
-                userCred = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.FromStream(stream).Secrets, Scopes, "user", CancellationToken.None, new FileDataStore(path, true)).Result;
-            }
-
-            string message = $"To: {TBTo.Text}\r\nSubject: {TBSubject.Text}\r\nContent-Type: text/html;charset=utf-8\r\n\r\n<h1>{TBMessage.Text}</h1>";
-            // call Gmail service
-            var service = new GmailService(new BaseClientService.Initializer() { HttpClientInitializer = userCred, ApplicationName = ApplicationName });
-            var msg = new Google.Apis.Gmail.v1.Data.Message();
-            msg.Raw = Base64UrlEncode(message.ToString());
-            service.Users.Messages.Send(msg, "me").Execute();
-            newAgent.Characters["MyBuddy"].Speak("I sent the e-mail. Anything else I can do for you, Dad?");
+            newAgent.Characters["MyBuddy"].Speak("I really should figure out how to send emails, huh?");
         }
     }
 }
